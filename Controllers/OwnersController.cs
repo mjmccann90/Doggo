07d86fca208fb4c73057498adeaebf3ef4319fc1
Mark McCann <mjmccann12@me.com>
2020-06-21 20:50:09 -0500

    crud push

---
+++ Controllers/OwnersController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Doggo.Models;
+using Doggo.Models.ViewModels;
+using Doggo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Doggo.Controllers
+{
+    public class OwnersController : Controller
+    {
+        private readonly OwnerRepository _ownerRepo;
+        private readonly DogRepository _dogRepo;
+        private readonly WalkerRepository _walkerRepo;
+        private readonly NeighborhoodRepository _neighborhoodRepo;
+
+        public OwnersController(IConfiguration config)
+        {
+            _ownerRepo = new OwnerRepository(config);
+            _dogRepo = new DogRepository(config);
+            _walkerRepo = new WalkerRepository(config);
+            _neighborhoodRepo = new NeighborhoodRepository(config);
+        }
+
+        // GET: OwnersController
+        public ActionResult Index()
+        {
+            List<Owner> owners = _ownerRepo.GetAllOwners();
+
+            return View(owners);
+        }
+
+        // GET: OwnersController/Details/5
+        public ActionResult Details(int id)
+        {
+            ProfileViewModel vm = new ProfileViewModel();
+            vm.Owner = _ownerRepo.GetOwnerById(id);
+
+            if (vm.Owner == null)
+            {
+                return NotFound();
+            }
+
+            vm.Dogs = _dogRepo.GetDogsByOwnerId(id);
+            vm.Walkers = _walkerRepo.GetWalkersInNeighborhood(vm.Owner.NeighborhoodId);
+
+            return View(vm);
+        }
+
+        // GET: Owners/Create
+        public ActionResult Create()
+        {
+            List<Neighborhood> neighborhoods = _neighborhoodRepo.GetAll();
+
+            OwnerFormViewModel vm = new OwnerFormViewModel()
+            {
+                Owner = new Owner(),
+                Neighborhoods = neighborhoods
+            };
+
+            return View(vm);
+        }
+
+        // POST: OwnersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Owner owner)
+        {
+            try
+            {
+                _ownerRepo.AddOwner(owner);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                // If something goes wrong, just keep the user on the same page so they can try again
+                return View(owner);
+            }
+        }
+
+        // GET: Owners/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            return View(owner);
+        }
+
+        // POST: OwnersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Owner owner)
+        {
+            try
+            {
+                _ownerRepo.UpdateOwner(owner);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(owner);
+            }
+        }
+
+        // GET: OwnersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+            return View(owner);
+        }
+
+        // POST: OwnersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Owner owner)
+        {
+            try
+            {
+                _ownerRepo.DeleteOwner(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                // If something goes wrong, just keep the user on the same page so they can try again
+                return View(owner);
+            }
+        }
+    }
+}
